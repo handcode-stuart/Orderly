54cc0b7e2508423bacaf06a9517ab91e335eadb4
Stuart Morris <admin@handcode.co.uk>
2019-06-12 21:49:26 +0100

    Added checked style to TaskItem

--- client/src/components/TaskItem/TaskItem.js
+++ client/src/components/TaskItem/TaskItem.js
@@ -23,7 +23,7 @@ const TaskItem = ({ completeTask, deleteTask, task }) => {
     };
 
     return (
-        <li className='task-item'>
+        <li className={completed ? "task-item  checked" : "task-item"}>
             <div className='task-item__wrapper'>
                 <div className='task-item__form-group'>
                     <span onClick={e => onCompleteClick()} className={completed ? "checked" : ""} />
